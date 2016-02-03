6c814fcdc25cf93763bbffae3cb2b0e442c761cf
Lauri Valma <laurivalma@users.noreply.github.com>
2016-02-03 10:40:18 +0200

    added calculator

---
+++ calc.js
@@ -0,0 +1,18 @@
+
+window.onload = function(){
+
+
+  //saan kätte väljad
+  var cm = document.getElementById('cm');
+  var inch = document.getElementById('inch');
+
+  //lisan nuppudele kuularid
+  document.getElementById('to_inch').addEventListener('click', convertToInch);
+  document.getElementById('to_cm').addEventListener('click', function(){
+    cm.value = inch.value*2.54;
+  });
+};
+
+function convertToInch() {
+  inch.value = cm.value/2.54;
+}
